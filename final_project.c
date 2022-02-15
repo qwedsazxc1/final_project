f6f58c474816b8edf1058a4d3c380f066ec25e13
qwedsazxc1 <linyihong1234@gmail.com>
2022-02-15 14:30:19 +0800

    rem token

--- final_project.c
+++ final_project.c
@@ -1,3 +1,5 @@
+// token : vscode://vscode.github-authentication/did-authenticate?windowid=1&code=43c2bd640855d0e60985&state=ffa64612-150a-4537-8fba-948b5b36e4b7
+
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
