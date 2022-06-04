ced92f29b4589cef1758e6b06bae12c116619603
keep1earning <linyihong1234@gmail.com>
2022-06-04 08:02:53 +0800

    debug test_case_generator.c

--- test_case_generator.c
+++ test_case_generator.c
@@ -9,41 +9,41 @@ int main(int argc, char *argv[]){
         printf("Usage : ./test_case_generator [number_of_test_case] [generate_file_name] ([parameter])\n");
         return 0;
     }
-	if (argc == 3){
-    	srand(time(NULL));
-    	int number_of_test_case = atoi(argv[1]);
-    	if (number_of_test_case <= 0){
-        	fprintf(stderr, "test case amount can not less than 1\n");
-        	return 0;
-    	}
-    	int student_id_list[] = {
-        	#include "student_id_list.txt"
-    	};
-    	int student_id_list_size = 12000;
-    	unsigned long long current_time = time(NULL);
 
-    	errno = 0;
-    	FILE *generate_file_name = fopen(argv[2], "w");
-    	if (generate_file_name == NULL){
-    	    perror("fopen");
-    	    return 0;
-    	}
-
-    	fprintf(generate_file_name, "time,student_id,place_id\n");
-		if(argc == 4 && strncmp(argv[3],"-l",2) == 0){
-			int a_randon_student_id = student_id_list[rand() % student_id_list_size];
-			int a_randon_place_id = rand() % 10000 + 1;
-			for (unsigned long long i = 0; i < number_of_test_case; i++){
-   	    		fprintf(generate_file_name, "%llu,%d,%d\n", current_time + i, a_randon_student_id, a_randon_place_id);
-    		}
-		}
-		else{
-			for (unsigned long long i =0; i < number_of_test_case; i++){
-				fprintf(generate_file_name, "%llu,%d,%d\n", current_time + i, student_id_list[rand() % student_id_list_size], rand() % 10000 + 1);
-			}
-		}	
-
-    	fclose(generate_file_name);
-    	return 0;
+	srand(time(NULL));
+	int number_of_test_case = atoi(argv[1]);
+	if (number_of_test_case <= 0){
+		fprintf(stderr, "test case amount can not less than 1\n");
+		return 0;
 	}
+	int student_id_list[] = {
+		#include "student_id_list.txt"
+	};
+	int student_id_list_size = 12000;
+	unsigned long long current_time = time(NULL);
+
+	errno = 0;
+	FILE *generate_file_name = fopen(argv[2], "w");
+	if (generate_file_name == NULL){
+		perror("fopen");
+		return 0;
+	}
+
+	fprintf(generate_file_name, "time,student_id,place_id\n");
+	if(argc == 4 && strcmp(argv[3],"-l") == 0){
+		int a_randon_student_id = student_id_list[rand() % student_id_list_size];
+		int a_randon_place_id = rand() % 10000 + 1;
+		for (unsigned long long i = 0; i < number_of_test_case; i++){
+			fprintf(generate_file_name, "%llu,%d,%d\n", current_time + i, a_randon_student_id, a_randon_place_id);
+		}
+	}
+	else{
+		for (unsigned long long i =0; i < number_of_test_case; i++){
+			fprintf(generate_file_name, "%llu,%d,%d\n", current_time + i, student_id_list[rand() % student_id_list_size], rand() % 10000 + 1);
+		}
+	}	
+
+	fclose(generate_file_name);
+	return 0;
+	
 }
