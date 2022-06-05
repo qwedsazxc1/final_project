8c62b4385e7492a47fc880b343aa25317ff0042c
yansawu <justinwu105@gmail.com>
2022-06-06 00:02:01 +0800

    add check_sort.c

---
+++ check_sort.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib,h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+	if(argc != 3){
+		printf("input: [check_file] [number_of_test_case] [parameter]\n")
+		return 0;
+	}
+	
+	if(atoi(argv[1]) <= 0){
+		printf("number_of_tset_can't_be_less_than_0\n")
+		return 0;
+	}
+	
+	int number_of_test_case = atoi(argv[1]);
+
+	int student_id = 0, prev_student_id = 0;
+	unsigned long long time = 0, prev_time = 0;
+	int place_id = 0, prev_place_id = 0;
+	if(strcmp(argv[2], -t) == 0){
+		for(int i = 0;i < number_of_test_case;i++){
+			scanf("%d,%llu,%d\n", &student_id, &time, &place_id);
+			if(i == 0){
+				//prev_student_id = student_id;
+				prev_time = time;
+				//prev_place_id = place_id;
+			}
+			if(prev_time > time){
+				printf("False\n");
+				return 0;
+			}
+			prev_time = time;
+		}
+	}
+	else if(strcmp(argv[2], -p) == 0){
+		for(int i = 0;i < number_of_test_case;i++){
+			scanf("%d,%llu,%d\n", &student_id, &time, &place_id);
+			if(i == 0){
+				prev_place_id = place_id;
+			}
+			if(prev_place_id > place_id){
+				printf("False\n");
+				return 0;
+			}
+			prev_place_id = place_id;
+		}
+
+	}
+	else if(strcmp(argv[2], -i) == 0){
+		for(int i = 0;i < number_of_test_case;i++){
+			scanf("%d,%llu,%d\n", &student_id, &time, &place_id);
+			if(i == 0){
+				prev_student_id = student_id;
+			}
+			if(prev_student_id > student_id){
+				printf("False\n");
+				return 0;
+			}
+			prev_student_id = student_id;
+		}
+
+	}
+	else return 0;
+}
+
