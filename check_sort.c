1ff9adb469bfe0f6ba52d4a438750238e25bf797
yansawu <justinwu105@gmail.com>
2022-06-06 00:08:10 +0800

    debug and add commemt in check_sort.c

--- check_sort.c
+++ check_sort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib,h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]){
 	if(argc != 3){
-		printf("input: [check_file] [number_of_test_case] [parameter]\n")
+		printf("input format: [check_file] [number_of_test_case] [parameter]\n")
 		return 0;
 	}
 	
@@ -18,13 +18,12 @@ int main(int argc, char *argv[]){
 	int student_id = 0, prev_student_id = 0;
 	unsigned long long time = 0, prev_time = 0;
 	int place_id = 0, prev_place_id = 0;
+	//check time sort
 	if(strcmp(argv[2], -t) == 0){
 		for(int i = 0;i < number_of_test_case;i++){
 			scanf("%d,%llu,%d\n", &student_id, &time, &place_id);
 			if(i == 0){
-				//prev_student_id = student_id;
 				prev_time = time;
-				//prev_place_id = place_id;
 			}
 			if(prev_time > time){
 				printf("False\n");
@@ -33,6 +32,7 @@ int main(int argc, char *argv[]){
 			prev_time = time;
 		}
 	}
+	//check place_id sort
 	else if(strcmp(argv[2], -p) == 0){
 		for(int i = 0;i < number_of_test_case;i++){
 			scanf("%d,%llu,%d\n", &student_id, &time, &place_id);
@@ -47,6 +47,7 @@ int main(int argc, char *argv[]){
 		}
 
 	}
+	//check student_id sort
 	else if(strcmp(argv[2], -i) == 0){
 		for(int i = 0;i < number_of_test_case;i++){
 			scanf("%d,%llu,%d\n", &student_id, &time, &place_id);
