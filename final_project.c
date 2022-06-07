74078ddaebbc84d125c30f83b74d0f6c9f063091
yansawu <justinwu105@gmail.com>
2022-06-08 00:28:23 +0800

    add parameter to the final_project.c

--- final_project.c
+++ final_project.c
@@ -59,9 +59,24 @@ void record(student_list student_list, place_list place_list);
 void delete(student_list student_list, place_list place_list);
 
 int main(int argc, char *argv[]){
-    if (argc != 1 && argc != 2){
-        printf("Usage : ./avl_tree_ver [file]\n");
+    if (argc != 1 && argc != 2 && argc != 3){
+        printf("Usage : ./avl_tree_ver [file] [parameter]\n");
         return 0;
+    }
+
+    if(argc == 3){
+        if(strcmp(argv[2], "-r") == 0){
+            long long unsigned student_id;
+            int time;
+            int place_id;
+            while(scanf("%llu,%d,%d\n", &student_id, &time, &place_id) != EOF){
+                printf("%llu,%d,%d", student_id, time, place_id);
+            }
+        }
+        else{
+            printf("input parameter false");
+            return 0;
+        }
     }
 
     errno = 0;
